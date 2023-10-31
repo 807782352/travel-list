6226ffc694c544c8f36bf947e5e12758522a40ea
z297xu <kyrie807782352@gmail.com>
2023-10-31 14:41:03 -0400

    add sorting

--- src/components/Stats/index.jsx
+++ src/components/Stats/index.jsx
@@ -1,9 +1,25 @@
-import React from 'react'
+import React from "react";
 
-export default function Stats() {
+export default function Stats({ items }) {
+  const numItems = items.length;
+  const numPacked = items.filter((item) => item.packed).length;
+  const percentPacked = Math.round((numPacked / numItems) * 100);
+
+  if (numItems === 0) {
+    return (
+      <footer className="stats">
+        <em>Add some items for preparation 👜 !!!</em>
+      </footer>
+    );
+  }
+
   return (
-    <footer className='stats'>
-        <em>💼 You have X items on your list, and you already packed X (X%) </em>
+    <footer className="stats">
+      <em>
+        {numItems === numPacked
+          ? "You are ready to go ✈️!!!"
+          : `💼 You have ${numItems} items on your list, and you already packed ${numPacked} (${percentPacked}%) `}
+      </em>
     </footer>
-  )
+  );
 }
